9223f05f539104f81bc9244f8ca10c28677f0f42
maturaofficial <maturaofficial@gmail.com>
2023-01-11 20:48:44 +0800

    update

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import { Container, Grid, Paper,TextField} from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import {  signInWithEmailAndPassword   } from 'firebase/auth';
 import { auth } from '../../firebase';
 import { NavLink, useNavigate } from 'react-router-dom'
@@ -8,7 +12,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
        
-    const onLogin = (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -26,62 +30,94 @@ const Login = () => {
     }
  
     return(
-        <>
-            <main >        
-                <section>
-                    <div>                                            
-                        <p> Kodego Login </p>                       
-                                                       
-                        <form>                                              
-                            <div>
-                                <label htmlFor="email-address">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"                                    
-                                    required                                                                                
-                                    placeholder="Email address"
-                                    onChange={(e)=>setEmail(e.target.value)}
-                                />
-                            </div>
-
-                            <div>
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"                                    
-                                    required                                                                                
-                                    placeholder="Password"
-                                    onChange={(e)=>setPassword(e.target.value)}
-                                />
-                            </div>
-                                                
-                            <div>
-                                <button                                    
-                                    onClick={onLogin}                                        
-                                >      
-                                    Login                                                                  
-                                </button>
-                            </div>                               
-                        </form>
-                       
-                        <p className="text-sm text-white text-center">
-                            No account yet? {' '}
-                            <NavLink to="/signup">
-                                Sign up
-                            </NavLink>
-                        </p>
-                                                   
-                    </div>
-                </section>
-            </main>
-        </>
-    )
+        <div>
+        <Container maxWidth="sm">
+          <Grid
+            container
+            spacing={1}
+            direction="column"
+            justifyContent="center"
+            style={{ minHeight: "80vh" }}
+          >
+              <Paper elevation={5} sx={{ padding: 4 }}>
+              <form onSubmit={handleLogin}>
+                <Grid container direction="column" spacing={0.5}>
+                  <Grid item>
+                    <h1>Log in</h1>
+                  </Grid>
+                  <Grid item>
+                    <h4>Username</h4>
+                    <TextField
+                      type="email"
+                      fullWidth
+                      label="Username"
+                      placeholder="Email address"
+                      variant="outlined"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <h4>Password</h4>
+                    <TextField
+                      type="password"
+                      fullWidth
+                      label="Password"
+                      placeholder="Password"
+                      variant="outlined"
+                      onChange={(e) => setPassword(e.target.value)}
+                      // InputProps={{
+                      //   endAdornment:(
+                      //       <InputAdornment position="end">
+                      //           <IconButton
+                      //           aria-label="toggle password"
+                      //           edge="end">
+                      //           <VisibilityIcon/>
+                      //           </IconButton>
+                      //       </InputAdornment>
+                      //   )
+                      // }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      style={{
+                        borderRadius: 5,
+                        backgroundColor: "#2196f3",
+                        padding: "18px, 36px",
+                        fontsize: "18px",
+                        fontWeight: "bold",
+                      }}
+                      variant="contained"
+                      type="submit"
+                      sx={{ mt: 3, width: 100 }}
+                    >
+                      LOGIN
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <FormControlLabel
+                      control={<Checkbox defaultChecked />}
+                      label="Remember me"
+                    />
+                  </Grid>
+                  <Grid item>
+                    <h4
+                      style={{
+                        color: "#2196f3",
+                      }}
+                    >
+                      <NavLink to="/signup">
+                        Sign up
+                      </NavLink>
+                    </h4>
+                  </Grid>
+                </Grid>
+              </form>
+            </Paper>
+          </Grid>
+        </Container>
+      </div>
+    );
 }
  
 export default Login
